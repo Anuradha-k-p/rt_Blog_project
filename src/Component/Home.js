e1a5243a2ce5e7507091d9990e2cddd9306afa4e
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-10-21 00:40:08 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,17 +5,15 @@ import "./Style.css"
 
 
 
-function Hollywood(){
- const [ContextData] = useContext(Store);
- console.log(ContextData);
+
+function Home(){
+  const [ContextData] = useContext(Store);
+  console.log(ContextData);
 
   return(
     <>
-     <div id="scroll-container">
-
-    <h1 id="scroll-text">Hollywood.......</h1>
-</div>
-    {/* {ContextData.filter((item)=> item.Category === "Hollywood")
+    
+    {/* {ContextData.filter((item)=> item.Category === "Home")
     
     .map((item,index)=>{
       return(
@@ -29,12 +27,41 @@ function Hollywood(){
       )
 
     })} */}
+        
 
+      
+      <div className="Titleimages">
+        <div className="bigimage">
+          <img
+            src="https://media.tomtom.com/m/2260170723d81048/original/Peugeot3008-CarLaunch-HPSlider-Desktop-1920x700px.jpg"
+            alt="not found"
+          ></img>
+        </div>
 
+        <div className="smallimage">
+          <img
+            src="https://www.foodfood.com/blog/wp-content/uploads/2022/05/Latest-Indian-Wedding-Food-Menu-List.jpg"
+            alt="not found"
+            height="240px"
+            width="500px"
+          ></img>
+
+          <img
+            src="https://lh3.googleusercontent.com/iN2Dkn4l9yoWKx0FMcIi2_1qzfa0MK_BKzeH7IC__wwMhO1vfOLBBangUteiJnIgpbV7AiEHc3mbhXNDLMJOaYAuH_9midBGd4fCI_P0=h450-rw"
+            alt="not found"
+            height="240px"
+            width="500px"
+          ></img>
+        </div>
+      </div>
+
+      <div className="borderbottom">
+      <h1 className="side">Latest</h1>
+      </div>
    
 
       <div className="middlecontainer">
-        {ContextData.filter((item) =>  item.id >= 26 && item.id <= 28 && item.Category === "Hollywood"  )
+        {ContextData.filter((item) =>  item.id >= 84 && item.id <= 86 && item.Category === "Home"  )
           .map((item, index) => {
             return (
               <div className="middlecontainerimage" key={index}>
@@ -56,77 +83,86 @@ function Hollywood(){
           })}
       </div>
 
-    
-    
-<div className="maincontainer">
-      
+
+      <div className="maincontainer">
 
         <div id="Lparent1">
-          <h1 className="side side2">Latest Articles</h1>
-          {ContextData.filter((item) => item.Category==="Hollywood")
+          <h1 className="side">Latest Articles</h1>
+          {ContextData.filter((item) => item.id >= 76 && item.id <= 84 &&item.Category === "Home")
 
             .map((item, index) => {
               return (
                 <div key={index}>
-                    <NavLink to={`/detail/${item.id}`} className="linkdes">
+                     <NavLink to={`/detail/${item.id}`} className="linkdes">
+                
                   <div id="parent2">
+                    
                     <img
-                      id="leftsideimage"
+                      id="leftsideimage "
+                      className="imageleftside"
                       src={item.image}
                       alt="not found"
                       height="100px"
-                      width="160px"
+                      width="180px"
                     />
-                    
-                    <div id="Description3">
-                      <h4>{item.Title}</h4>
-                      <p>{item.Description.slice(0,95)+"..."}</p>
+                   
+                    <div id="Description2 " className="home-left-description">
+                    <h4>{item.Title}</h4> 
+                      <p>{item.Description.slice(0,110)+"..."}</p>
                     </div>
                   </div>
                   </NavLink>
                 </div>
               );
             })}
-          
+       
         </div>
 
     <div className="middlecontainerright">
-    
      
+      
         <div className="Rparent1">
-          <h1 className="side2 side">Top Post</h1>
-        
-
-        
-
+          <h1 className="side">Top Post</h1>
+         
           {ContextData
             .filter(
-              (item) => item.id >= 24 && item.id <= 27 && item.Category === "Hollywood"
+              (item) => item.id >= 80 && item.id <= 83 && item.Category === "Home"
             )
             .map((item, index) => {
               return (
-                <div id="parent2" key={index}>
-                <NavLink to={`/detail/${item.id}`} className="linkdes linkdes3">
-               <img
-                 src={item.image}
-                 alt="not found"
-                 height="100px"
-                 width="130px"
-               />
-               <div id="Description">
-                 <p>{item.Description.slice(0,100)+"..."}</p>
-               </div>
-               <h3 id="number">{index+1}</h3>
-               </NavLink>
-             </div>
+                <div id="parent2" className="parent2-top-post" key={index}>
+                     <NavLink to={`/detail/${item.id}`} className="linkdes">
+                  <img
+                    src={item.image}
+                    alt="not found"
+                    height="130px"
+                    width="210px"
+                    id="rightsideimage"
+                    className="homerightsideimage"
+                  />
+                  <div className="rightsidetitle-decription">
+              <h3 className="center">{item.Title}</h3>
+                  <div id="Description"  className="home-right-description">
+                    <p>{item.Description.slice(0,90)}</p>
+                    <h3 id="number">{index+1}</h3>
+                  </div>
+                  </div>
+                  </NavLink>
+                </div>
               );
             })}
         </div>
-        <div className="Advertise2 side2"><h2 className="Adtext2">Advertisement</h2></div>
-        </div>
-        </div>
+        <div className="Advertise"><h2 className="Adtext">Advertisement</h2></div>
+    </div>
+      </div>
+
+
+
+
+
+
     </>
   )
 }
 
-export default Hollywood
+export default Home
